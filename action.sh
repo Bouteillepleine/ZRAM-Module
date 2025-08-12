4af102f5c41e980ad8c9d4dc97a03f7d0e7bfafd
Bouteillepleine <76688492+Bouteillepleine@users.noreply.github.com>
2025-08-12 14:29:27 +0200

    Update action.sh

--- action.sh
+++ action.sh
@@ -1,27 +1,27 @@
 MODDIR="/data/adb/modules/ZRAM-Module"
 CONFIG_FILE="$MODDIR/config.prop"
 
-# 加载配置
+# Load configuration
 if [ -f "$CONFIG_FILE" ]; then
   . "$CONFIG_FILE"
 else
-  echo "未找到 config.prop，退出" >&2
+  echo "config.prop not found, exiting" >&2
   exit 1
 fi
 
-# 重载 zram
-echo "停用当前 zram..."
+# Reload zram
+echo "Disabling current zram..."
 swapoff /dev/block/zram0
 
-echo "重置 zram 参数..."
+echo "Resetting zram parameters..."
 echo 1 > /sys/block/zram0/reset
 echo 0 > /sys/block/zram0/disksize
 echo 8 > /sys/block/zram0/max_comp_streams
 echo "$ZRAM_ALGO" > /sys/block/zram0/comp_algorithm
 echo "$ZRAM_SIZE" > /sys/block/zram0/disksize
 
-echo "创建 zram 并启用..."
+echo "Creating zram and enabling..."
 mkswap /dev/block/zram0
 swapon /dev/block/zram0
 
-echo "ZRAM 热重载完成。"
+echo "ZRAM hot reload completed."
